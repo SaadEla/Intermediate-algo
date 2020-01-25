ae3fc16eda126caefa0a45295f9a892f63980273
Elaouani Saad <45466806+SaadEla@users.noreply.github.com>
2020-01-25 14:52:51 +0100

    Add files via upload

---
+++ Intermediate algo II.js
@@ -0,0 +1,11 @@
+function spinalCase(str) {
+  // Replace low-upper case to low-space-uppercase
+  str = str.replace(/([a-z])([A-Z])/g, "$1 $2");
+  // Split on whitespace and underscores and join with dash
+  return str
+    .toLowerCase()
+    .split(/(?:_| )+/)
+    .join("-");
+}
+
+spinalCase('This Is Spinal Tap');
