d982c28a548b48f351904db8909949a12ed9a456
Elaouani Saad <45466806+SaadEla@users.noreply.github.com>
2020-01-25 14:37:56 +0100

    Add files via upload

---
+++ Intermediate algo I.js
@@ -0,0 +1,14 @@
+function whatIsInAName(collection, source) {
+  // "What's in a name? that which we call a rose
+  // By any other name would smell as sweet.”
+  // -- by William Shakespeare, Romeo and Juliet
+  var srcKeys = Object.keys(source);
+
+  return collection.filter(function(obj) {
+    return srcKeys.every(function(key) {
+      return obj.hasOwnProperty(key) && obj[key] === source[key];
+    });
+  });
+}
+
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
